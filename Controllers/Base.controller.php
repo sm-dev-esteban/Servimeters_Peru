680786db1ea6ccff95557d5cb9dda8f637e2c235
William Ricardo Enciso Bautista <william.enciso@servimeterssa.com>
2023-06-20 12:32:20 -0500

    BASE

---
+++ Controllers/Base.controller.php
@@ -0,0 +1,90 @@
+<?php
+
+class BaseController
+{
+
+    private static $model;
+
+    public static function setModel($model)
+    {
+        self::$model = $model;
+    }
+
+    /**
+     * @param mixed $action
+     * 
+     * @return mixed
+     */
+    public static function execute($action)
+    {
+        self::$model->$action(self::getObjectModel());
+    }
+
+    /**
+     * @return array
+     */
+
+    private static function getObjectModel()
+    {
+        return self::$model->getObjectAsArray();
+    }
+
+    /**
+     * @return string
+     */
+    public static function getId()
+    {
+        return self::$model->getId();
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public static function insert()
+    {
+        return self::$model->insert(self::getObjectModel());
+    }
+
+    /**
+     * @return bool
+     */
+    public static function delete()
+    {
+        self::$model->delete(self::$model->getId());
+    }
+
+    /**
+     * @return bool
+     */
+    public static function update()
+    {
+        self::$model->update(self::getObjectModel());
+    }
+
+    /**
+     * @return object
+     */
+    public static function get()
+    {
+        self::$model->get(self::$model->getId());
+    }
+
+    /**
+     * @return array
+     */
+    public static function getAll()
+    {
+        return self::$model->getAll();
+    }
+
+    /**
+     * @param mixed $condition
+     * 
+     * @return array
+     */
+    public static function getCondition($condition)
+    {
+        return self::$model->getCondition($condition);
+    }
+}
