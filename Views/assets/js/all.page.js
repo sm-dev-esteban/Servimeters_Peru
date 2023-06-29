a9640dcfbb70dffa0c36d88e6e8bb989388659c3
William Ricardo Enciso Bautista <william.enciso@servimeterssa.com>
2023-06-29 17:40:21 -0500

    Fixed Issues

--- Views/assets/js/all.page.js
+++ Views/assets/js/all.page.js
@@ -89,14 +89,14 @@ $(":checkbox[data-bootstrap-switch]").each(function () {
 // TOAST
 //------------------------------------------------------------------------------------------------------------------------------------------------------
 
-var Toast = Swal.mixin({
-    toast: true,
-    position: 'top-end',
-    showConfirmButton: false,
-    timer: 3000
-});
-
 function showToast(msg = 'Servimeters', icon = 'info') {
+    var Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000
+    });
+    
     Toast.fire({
         icon: icon,
         title: msg 
